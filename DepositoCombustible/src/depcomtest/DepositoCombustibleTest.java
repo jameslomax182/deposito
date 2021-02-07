66b6b61a5691bcbb8d303b3bfd6dd15f6732b784
jameslomax182 <jameslomax182@hotmail.com>
2021-02-07 14:24:40 +0100

    test1
    pruebas

---
+++ DepositoCombustible/src/depcomtest/DepositoCombustibleTest.java
@@ -0,0 +1,44 @@
+package depcomtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustibleTest {
+
+	@Test
+	public void testDepositoCombustible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepositoNivel() {
+		DepositoCombustibleTest tank = new DepositoCombustibleTest(40.0,0.0);
+	}
+
+	@Test
+	public void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
