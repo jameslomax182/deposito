9cbb53ffc2b1206fcf3f61a2880c839b212f7682
jameslomax182 <jameslomax182@hotmail.com>
2021-02-08 22:39:59 +0100

    Create DepositoCombustiblePrueba.java
    otro intento de pruebas

---
+++ DepositoCombustible/src/DepositoCombustiblePrueba.java
@@ -0,0 +1,105 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustiblePrueba {
+
+	@Test
+	public final void testDepositoCombustible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetDepositoNivel() {
+		double valorNivel=0;
+		DepositoCombustible tank = new DepositoCombustible(40.0,0.0);
+		valorNivel = tank.GetDepositoNivel();
+		assertEquals(0,valorNivel,0.01);
+	}
+
+	@Test
+	public final void testGetDepositoMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testEstaVacio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testEstaLleno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
